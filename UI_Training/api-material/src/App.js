f41ff84300a9745fefbc473751c4694f2170db5c
Yash Bagri <ybagri@primesoft.net>
2021-11-16 15:03:58 -0600

    made the api task

---
+++ UI_Training/api-material/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from "./components/header_components";
+import Footer from "./components/footer_components";
+import logo from './logo.svg';
+import './App.css';
+import Body from "./components/body_components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
