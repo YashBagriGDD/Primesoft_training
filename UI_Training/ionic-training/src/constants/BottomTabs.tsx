1b2c84be293cf8f0c664156f115cc3bf53c20794
VENGEANCE\ybagr <ybagri>
2021-12-12 00:23:34 -0600

    Made Tabs component

---
+++ UI_Training/ionic-training/src/constants/BottomTabs.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {IonIcon, IonLabel, IonTabBar, IonTabButton} from "@ionic/react";
+import {gameController, musicalNote, videocam} from "ionicons/icons";
+
+const BottomTabs: React.FC = () => {
+    return(
+        <IonTabBar slot={"bottom"}>
+            <IonTabButton tab={"music"}>
+                <IonLabel>Music</IonLabel>
+                <IonIcon ios={musicalNote} md={musicalNote}></IonIcon>
+            </IonTabButton>
+            <IonTabButton tab={"movies"}>
+                <IonLabel>Movies</IonLabel>
+                <IonIcon ios={videocam} md={videocam}></IonIcon>
+            </IonTabButton>
+            <IonTabButton>
+                <IonLabel>Games</IonLabel>
+                <IonIcon ios={gameController} md={gameController}></IonIcon>
+            </IonTabButton>
+        </IonTabBar>
+    );
+}
+
+export default BottomTabs;
