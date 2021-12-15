e7c97a48e27e164f4a8838c88bede22de4bacf97
VENGEANCE\ybagr <ybagri>
2021-12-15 03:15:03 -0600

    Props on carousel working for the most part. Changes IonSlides to Swiper.js framework

--- UI_Training/ionic-training/src/components/common/Carousel.tsx
+++ UI_Training/ionic-training/src/components/common/Carousel.tsx
@@ -1,18 +1,49 @@
 import React from "react";
-import {IonSlide, IonSlides} from "@ionic/react";
 import Card, {CardInterface} from "./Card";
+import {Swiper, SwiperSlide} from "swiper/react/swiper-react";
+import {Mousewheel, Pagination} from "swiper";
 
-const Carousel: React.FC<any> = (props) => {
-    const slideItems = props.data.map((item: { header: string; content: string; [key: string]: any;}) => (
-        <IonSlide>
-            <Card header={item.header} content={item.content} />
-        </IonSlide>
-    ))
+import "swiper/swiper.min.css";
+// import 'swiper/modules/autoplay/autoplay.min.css';
+import "swiper/modules/mousewheel/mousewheel.min.css"
+// import 'swiper/modules/keyboard/keyboard.min.css';
+import 'swiper/modules/pagination/pagination.min.css';
+// import 'swiper/modules/scrollbar/scrollbar.min.css';
+// import 'swiper/modules/zoom/zoom.min.css';
+import "@ionic/react/css/ionic-swiper.css";
+
+interface Props {
+    cardData?: CardInterface[],
+    imgData?: string[],
+    type: string,
+}
+
+const Carousel: React.FC<Props> = (props) => {
+    let slideItems;
+
+    if (props.type.toLowerCase() == "card") {
+        slideItems = props.cardData?.map((item, index) => (
+            <SwiperSlide key={index}>
+                <Card header={item.header} content={item.content} subtitle={item.subtitle} img={item.img}/>
+            </SwiperSlide>
+        ));
+    } else if (props.type.toLowerCase() == "img") {
+        slideItems = props.imgData?.map((item, index) => (
+            <SwiperSlide key={index}>
+                <img src={item}/>
+            </SwiperSlide>
+        ))
+    }
 
     return (
-        <IonSlides>
+        <Swiper
+            modules={[Mousewheel, Pagination]}
+            mousewheel={true}
+            slidesPerView={2}
+            pagination={true}
+        >
             {slideItems}
-        </IonSlides>
+        </Swiper>
     );
 }
 
