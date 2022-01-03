28e7fb0af372f036b203beeb09eb0e17e386808d
Yash Bagri <ybagri@gmail.com>
2022-01-03 01:58:41 -0600

    testing git

--- UI_Training/ionic-training/src/components/common/BottomTabs.tsx
+++ UI_Training/ionic-training/src/components/common/BottomTabs.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import {IonIcon, IonLabel, IonTabBar, IonTabButton} from "@ionic/react";
-import {gameController, musicalNote, videocam} from "ionicons/icons";
+import { IonIcon, IonLabel, IonTabBar, IonTabButton } from "@ionic/react";
+import { gameController, musicalNote, videocam } from "ionicons/icons";
 
+//TODO: Delete this comment
 const BottomTabs: React.FC = () => {
-    return(
-        <IonTabBar slot={"bottom"}>
-            <IonTabButton tab={"music"}>
-                <IonLabel>Music</IonLabel>
-                <IonIcon ios={musicalNote} md={musicalNote}></IonIcon>
-            </IonTabButton>
-            <IonTabButton tab={"movies"}>
-                <IonLabel>Movies</IonLabel>
-                <IonIcon ios={videocam} md={videocam}></IonIcon>
-            </IonTabButton>
-            <IonTabButton tab={"games"}>
-                <IonLabel>Games</IonLabel>
-                <IonIcon ios={gameController} md={gameController}></IonIcon>
-            </IonTabButton>
-        </IonTabBar>
-    );
-}
+  return (
+    <IonTabBar slot={"bottom"}>
+      <IonTabButton tab={"music"}>
+        <IonLabel>Music</IonLabel>
+        <IonIcon ios={musicalNote} md={musicalNote}></IonIcon>
+      </IonTabButton>
+      <IonTabButton tab={"movies"}>
+        <IonLabel>Movies</IonLabel>
+        <IonIcon ios={videocam} md={videocam}></IonIcon>
+      </IonTabButton>
+      <IonTabButton tab={"games"}>
+        <IonLabel>Games</IonLabel>
+        <IonIcon ios={gameController} md={gameController}></IonIcon>
+      </IonTabButton>
+    </IonTabBar>
+  );
+};
 
 export default BottomTabs;
