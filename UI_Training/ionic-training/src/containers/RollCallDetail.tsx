f4ebb38ce23a3145dadb0fbc7af663f78e79280b
Yash Bagri <ybagri@gmail.com>
2022-01-04 00:28:39 -0600

    detail page structure created

--- UI_Training/ionic-training/src/containers/RollCallDetail.tsx
+++ UI_Training/ionic-training/src/containers/RollCallDetail.tsx
@@ -1,14 +1,26 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
+  IonCard,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
   IonContent,
+  IonFooter,
+  IonGrid,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { chevronBack } from "ionicons/icons";
-import React, { useEffect } from "react";
+import { chevronBack, chevronForward, pencil } from "ionicons/icons";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { RootState } from "..";
@@ -16,24 +28,18 @@ import { PostsState } from "../interfaces/interfaces";
 
 import "./RollCallDetail.css";
 
-interface RollCallParams {
+interface RollCallDetailParams {
   index: string;
 }
 
 const RollCallDetail: React.FC = () => {
-  const { index } = useParams<RollCallParams>();
+  const { index } = useParams<RollCallDetailParams>();
 
   const posts = useSelector<RootState, PostsState["posts"]>(
     (state) => state.posts
   );
 
   const currentRollCall = posts.list[parseInt(index)];
-
-  useEffect(() => {
-    console.log(index);
-    console.log(posts);
-    console.log(currentRollCall);
-  }, []);
 
   return (
     <IonPage id="details">
@@ -51,12 +57,92 @@ const RollCallDetail: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
-        <div>
-          <p>{currentRollCall.state}</p>
-          <p>{currentRollCall.billNum}</p>
-          <p>{currentRollCall.name}</p>
-        </div>
+        <IonGrid className={"ion-padding"}>
+          <IonRow>
+            <IonCol>
+              <IonCard color={"medium"}>
+                <IonCardSubtitle>
+                  <span>State - Bill Number</span>
+                </IonCardSubtitle>
+                <IonCardTitle>
+                  <span>
+                    {currentRollCall.state} - {currentRollCall.billNum}
+                  </span>
+                </IonCardTitle>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonCard>
+                <IonCardSubtitle>
+                  <span>Enter Test Roll Call Name</span>
+                </IonCardSubtitle>
+                <IonCardTitle>
+                  <span>{currentRollCall.name}</span>
+                </IonCardTitle>
+                <IonButton slot="end" color={"purple"}>
+                  <IonIcon icon={pencil} />
+                </IonButton>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonList>
+              <IonItem>
+                <IonLabel>
+                  <span>Summary</span>
+                </IonLabel>
+                <IonButton slot="end" fill="clear">
+                  <IonIcon icon={chevronForward} />
+                </IonButton>
+              </IonItem>
+              <IonItem>
+                <IonLabel>
+                  <span>View Location</span>
+                </IonLabel>
+                <IonButton slot="end" color="purple" shape="round">
+                  <IonIcon icon={pencil} />
+                </IonButton>
+              </IonItem>
+              <IonItem>
+                <IonLabel>
+                  <span>Assign Lobbyists</span>
+                </IonLabel>
+                <IonButton slot="end" color="purple" shape="round">
+                  <IonIcon icon={pencil} />
+                </IonButton>
+              </IonItem>
+              <IonItem>
+                <IonLabel>
+                  <span>View Timeline</span>
+                </IonLabel>
+                <IonButton slot="end" color="purple" shape="round">
+                  <IonIcon icon={pencil} />
+                </IonButton>
+              </IonItem>
+            </IonList>
+          </IonRow>
+        </IonGrid>
       </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="6">
+                <IonButton fill="outline" color="purple">
+                  Delete
+                </IonButton>
+              </IonCol>
+              <IonCol size="6">
+                <IonButton fill="solid" color="purple" disabled={true}>
+                  Initiated
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
