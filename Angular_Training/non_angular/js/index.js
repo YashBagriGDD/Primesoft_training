07543a83b21d1d9d463190f917bb12f46d3e1fac
YashBagriGDD <yxb1236@rit.edu>
2022-05-19 11:18:44 -0500

    Form added

---
+++ Angular_Training/non_angular/js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.onload = function () {
+  //Initialize forms
+  let forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (e) => {
+        if (form.checkValidity() == false) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+};
