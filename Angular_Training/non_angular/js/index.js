e183200e96088a4d7ce2e5677e3e405650a87ca1
YashBagriGDD <yxb1236@rit.edu>
2022-05-24 08:36:55 -0500

    table populate

--- Angular_Training/non_angular/js/index.js
+++ Angular_Training/non_angular/js/index.js
@@ -30,56 +30,56 @@ window.onload = function () {
 
   // https://stackoverflow.com/questions/51275730/populate-html-table-with-json-data
   //first add an event listener for page load
-  document.addEventListener("DOMContentLoaded", get_json_data, false); // get_json_data is the function name that will fire on page load
+  // document.addEventListener("DOMContentLoaded", get_json_data, false); // get_json_data is the function name that will fire on page load
 
-  //this function is in the event listener and will execute on page load
-  function get_json_data() {
-    // Relative URL of external json file
-    var json_url = "../assets/Data.json";
+  // //this function is in the event listener and will execute on page load
+  // function get_json_data() {
+  //   // Relative URL of external json file
+  //   var json_url = "../assets/Data.json";
 
-    //Build the XMLHttpRequest (aka AJAX Request)
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        //when a good response is given do this
+  //   //Build the XMLHttpRequest (aka AJAX Request)
+  //   xmlhttp = new XMLHttpRequest();
+  //   xmlhttp.onreadystatechange = function () {
+  //     if (this.readyState == 4 && this.status == 200) {
+  //       //when a good response is given do this
 
-        var data = JSON.parse(this.responseText); // convert the response to a json object
-        append_json(data); // pass the json object to the append_json function
-      }
-    };
-    //set the request destination and type
-    xmlhttp.open("POST", json_url, true);
-    //set required headers for the request
-    xmlhttp.setRequestHeader(
-      "Content-type",
-      "application/x-www-form-urlencoded"
-    );
-    // send the request
-    xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
-  }
+  //       var data = JSON.parse(this.responseText); // convert the response to a json object
+  //       append_json(data); // pass the json object to the append_json function
+  //     }
+  //   };
+  //   //set the request destination and type
+  //   xmlhttp.open("POST", json_url, true);
+  //   //set required headers for the request
+  //   xmlhttp.setRequestHeader(
+  //     "Content-type",
+  //     "application/x-www-form-urlencoded"
+  //   );
+  //   // send the request
+  //   xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
+  // }
 
-  //this function appends the json data to the table 'gable'
-  function append_json(data) {
-    var table = document.getElementById("tableBody");
-    data.forEach(function (object) {
-      var tr = document.createElement("tr");
-      tr.innerHTML =
-        "<td>" +
-        object.firstName +
-        "</td>" +
-        "<td>" +
-        object.lastName +
-        "</td>" +
-        "<td>" +
-        object.age +
-        "</td>" +
-        "<td>" +
-        object.height +
-        "</td>" +
-        "<td>" +
-        object.weight +
-        "</td>";
-      table.appendChild(tr);
-    });
-  }
+  // //this function appends the json data to the table 'gable'
+  // function append_json(data) {
+  //   var table = document.getElementById("tableBody");
+  //   data.forEach(function (object) {
+  //     var tr = document.createElement("tr");
+  //     tr.innerHTML =
+  //       "<td>" +
+  //       object.firstName +
+  //       "</td>" +
+  //       "<td>" +
+  //       object.lastName +
+  //       "</td>" +
+  //       "<td>" +
+  //       object.age +
+  //       "</td>" +
+  //       "<td>" +
+  //       object.height +
+  //       "</td>" +
+  //       "<td>" +
+  //       object.weight +
+  //       "</td>";
+  //     table.appendChild(tr);
+  //   });
+  // }
 };
--- Angular_Training/non_angular/js/indexJQuery.js
+++ Angular_Training/non_angular/js/indexJQuery.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
-  //   $("#table1").on("click", function () {
-  //     $("#table1").DataTable();
-  //   });
+  // Populate table with JSON
+  $.getJSON("../assets/Data.json", (data) => {
+    let items = [];
+    let line;
+    $.each(data, (index, value) => {
+      line = "<td class='table-item-first-name'>" + value.firstName + "</td>";
+      line += "<td class='table-item-last-name'>" + value.lastName + "</td>";
+      line += "<td class='table-item-age'>" + value.age + "</td>";
+      line += "<td class='table-item-height'>" + value.height + "</td>";
+      line += "<td class='table-item-weight'>" + value.weight + "</td>";
+
+      let $tr = $("<tr>").attr({ class: "table-row-item" }).append(line);
+      items.push($tr);
+    });
+
+    $("#tableBody").append(items);
+  }).fail(() => {
+    console.log("An error has ocurred");
+  });
 
   // Table sort from:
   // https://orangeable.com/javascript/jquery-table-sorting#:~:text=%20jQuery%20Table%20Sorting%3A%20Sort%20Data%20on%20Header,function%20call%20in%20our%20click%20event...%20More%20
-
   $(document).on("click", "table thead tr th:not(.no-sort)", function () {
     var table = $(this).parents("table");
     var rows = $(this)
