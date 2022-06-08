773f35cdcd1a74991b392b3ff67375327320d30a
Yash Bagri <shadowcero474@gmail.com>
2022-06-08 00:22:42 -0500

    filled out input atom

--- Angular_Training/angular-app/src/app/input-atom/input-atom.component.ts
+++ Angular_Training/angular-app/src/app/input-atom/input-atom.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-atom',
   templateUrl: './input-atom.component.html',
-  styleUrls: ['./input-atom.component.css']
+  styleUrls: ['./input-atom.component.css'],
 })
 export class InputAtomComponent implements OnInit {
+  @Input() name!: string;
+  @Input() type: string;
+  @Input() idName!: string;
+  @Input() placeholder: string;
+  @Input() isRequired: boolean;
+  @Input() value: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.type = 'text';
+    this.placeholder = '';
+    this.isRequired = true;
+    this.value = '';
   }
 
+  ngOnInit(): void {}
 }
