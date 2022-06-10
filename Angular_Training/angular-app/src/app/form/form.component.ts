d1287c6f5b6e2d9571095d1808b029eb2ac56d84
Yash Bagri <shadowcero474@gmail.com>
2022-06-10 08:06:41 -0500

    basic form validation

--- Angular_Training/angular-app/src/app/form/form.component.ts
+++ Angular_Training/angular-app/src/app/form/form.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  validateForm() {
+    let form = document.getElementById('signUpForm') as HTMLFormElement;
+    if (form.checkValidity() === false) {
+      event?.preventDefault();
+      event?.stopPropagation();
+    }
+    form.classList.add('was-validated');
   }
-
 }
