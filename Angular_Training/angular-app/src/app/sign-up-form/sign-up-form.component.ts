9e1b8a472ddf80cc04ff0c1418ebb149dd912bc4
Yash Bagri <shadowcero474@gmail.com>
2022-06-14 21:12:24 -0500

    custom pass validation working

--- Angular_Training/angular-app/src/app/sign-up-form/sign-up-form.component.ts
+++ Angular_Training/angular-app/src/app/sign-up-form/sign-up-form.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -10,22 +18,53 @@ export class SignUpFormComponent implements OnInit {
   signUpForm!: FormGroup;
   genders: string[] = ['Male', 'Female', 'Other'];
 
+  //regexp
+  oneLower: RegExp = /^(?=.*[a-z])/;
+  oneUpper: RegExp = /^(?=.*[A-Z])/;
+  oneDigit: RegExp = /^(?=.*\d)/;
+  oneSpecialChar: RegExp = /^(?=.*[\W])/;
+
   constructor() {}
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null),
-      username: new FormControl(null),
+      lastName: new FormControl(null, Validators.required),
+      username: new FormControl(null, Validators.required),
       password: new FormControl(null, [
         Validators.required,
         Validators.minLength(8),
+        this.testPassword(),
       ]),
-      gender: new FormControl('Other'),
+      gender: new FormControl('Other', Validators.required),
+      age: new FormControl(null),
+      address: new FormControl(null),
     });
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+  }
+
+  testPassword(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+
+      let errors = {};
+
+      const hasLower = this.oneLower.test(value);
+      const hasUpper = this.oneUpper.test(value);
+      const hasDigit = this.oneDigit.test(value);
+      const hasSpecial = this.oneSpecialChar.test(value);
+
+      if (hasLower && hasUpper && hasDigit && hasSpecial) return null;
+
+      if (!hasLower) errors = { ...errors, noLower: true };
+      if (!hasUpper) errors = { ...errors, noUpper: true };
+      if (!hasDigit) errors = { ...errors, noDigit: true };
+      if (!hasSpecial) errors = { ...errors, noSpecial: true };
+
+      return errors;
+    };
   }
 }
